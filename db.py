c8d25c198874a1b92bfd7251d6ac99ed105e4fa4
Harold Ospina <hadesosca@gmail.com>
2026-05-05 23:48:14 -0500

    errores corregidos

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 from sqlmodel import Session, create_engine, SQLModel
 from fastapi import FastAPI, Depends
@@ -6,18 +7,18 @@ from typing import Annotated
 
 load_dotenv()
 neon_db = os.getenv("DATABASE_URL_NEON")
-print ("DB URL;",  neon_db)
+print("DB URL;", neon_db)
 engine = create_engine(neon_db)
 
 def create_all_tables(app: FastAPI):
-    def create_all_tables(app: FastAPI):
-        from models.models import Motorbase, Motorid
-        from models.owner import Owner, Ownerid
-        if os.getenv("ENV") == "DEV":
-            SQLModel.metadata.create_all(engine)
-        yield
+    from models.models import Motorbase, Motorid
+    from models.owner import Owner, Ownerid
+    print("Creando tablas...")
+    SQLModel.metadata.create_all(engine)
+    print("Tablas creadas!")
     yield
-def get_session()->Session:
+
+def get_session() -> Session:
     with Session(engine) as session:
         yield session
 
